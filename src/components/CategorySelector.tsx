4e8d69474030882c2edd8d64a98fa38d5bdb9fdc
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-09-09 23:19:10 +0900

    refactor: move category selection into CategorySelector component

---
+++ src/components/CategorySelector.tsx
@@ -0,0 +1,18 @@
+import { useRecoilState } from 'recoil';
+import { Categories, categoryState } from '../atoms';
+
+export default function CategorySelector() {
+  const [category, setCategory] = useRecoilState(categoryState);
+
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as any);
+  };
+
+  return (
+    <select onInput={onInput} value={category}>
+      <option value={Categories.TO_DO}>To Do</option>
+      <option value={Categories.DOING}>Doing</option>
+      <option value={Categories.DONE}>Done</option>
+    </select>
+  );
+}
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,25 +1,17 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { categoryState, Cateogories, toDoSelector } from '../atoms';
+import { useRecoilValue } from 'recoil';
+import { toDoSelector } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
-import React from 'react';
+import CategorySelector from './CategorySelector';
 
 export default function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
-  const [category, setCategory] = useRecoilState(categoryState);
-  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(event.currentTarget.value as any);
-  };
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <select onInput={onInput} value={category}>
-        <option value={Cateogories.TO_DO}>To Do</option>
-        <option value={Cateogories.DOING}>Doing</option>
-        <option value={Cateogories.DONE}>Done</option>
-      </select>
+      <CategorySelector />
 
       <CreateToDo />
       {toDos?.map((item) => (
